84d24fa964596c4ba1a5e6bbd4f89327cf0325f1
Jose R F Junior <web2ajax@gmail.com>
2020-08-06 22:49:45 -0400

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
+import { AppComponent } from './app.component';
+import { DicomViewerModule } from 'ng-dicomviewer';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    CommonModule,
+    MatProgressSpinnerModule,
+    DicomViewerModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
